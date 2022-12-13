fd6b16862d4f81d7b5ad1f2efb0ac66a763fb48a
Karl Richer <karlricher@Karls-MacBook-Air.local>
2022-12-13 15:45:10 -0500

    Auth0 login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,37 @@
+import styled from "styled-components";
+
+import LoginButton from "./components/LoginButton";
+
 const App = () => {
-  return <h1>PowerHub</h1>;
+  return (
+    <Wrapper>
+      <SmallerWrapper>
+        <PowerHub>PowerHub</PowerHub>
+        <Paragraph>Connect with Powerlifters from around the world.</Paragraph>
+      </SmallerWrapper>
+
+      <LoginButton />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  height: 100vh;
+`;
+
+const SmallerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PowerHub = styled.h1``;
+
+const Paragraph = styled.p``;
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-9hpj3k23.us.auth0.com"
+    clientId="p09YDeI9xzt9PuFSKrkC1mQoiux3inLe"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
